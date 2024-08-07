5e745ef7a6b2cd02a167c50e17c71433ab5a9316
Urfan Musayev <urfanmusayev2003@gmail.com>
2024-08-07 15:17:28 +0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -12,4 +12,5 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    port = os.environ.get('PORT', '2023')
     execute_from_command_line(sys.argv)
